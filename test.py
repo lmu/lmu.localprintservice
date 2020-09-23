5af9ad5dcb7c585b5f02a241de53354bff9f29bf
Alexander Loechel <Alexander.Loechel@lmu.de>
2020-09-24 00:57:01 +0200

    cleanup test

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 #!./bin/python
 
-import base64
 import requests
 
 
-url = "http://localhost:6543/api/v1/printpdf"
+url = "http://localhost:6543/api/v1/print"
 
 with open("doc.pdf", "rb") as input:
     data = input.read()
-    #data = base64.b64encode(data)
     r = requests.request("PUT", url, data=data, headers={"Content-Type": "application/pdf"})
     print(r)
